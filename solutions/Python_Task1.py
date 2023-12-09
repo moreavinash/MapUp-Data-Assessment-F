5bf88e1746bb62dcdca275cde1a6c005338bb738
avinash <avinashmoreavii@gmail.com>
2023-12-10 01:31:58 +0530

    mfc

---
+++ solutions/Python_Task1.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+def generate_car_matrix(dataframe):
+    # Pivot the DataFrame to have id_2 values as columns, id_1 values as index, and car values as data
+    result_df = dataframe.pivot(index='id_1', columns='id_2', values='car')
+
+    # Fill NaN values with 0
+    result_df = result_df.fillna(0)
+
+    # Set diagonal values to 0
+    for idx in result_df.index:
+        result_df.at[idx, idx] = 0
+
+    return result_df
+
+# Example usage
+# Assuming you have loaded your dataset-1.csv into a DataFrame named df
+df = pd.read_csv('dataset-1.csv')
+
+# Generate the car matrix using the defined function
+result_matrix = generate_car_matrix(df)
+
+# Display the result
+print(result_matrix)
